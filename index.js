1d51660cf6a9270a2a76fcbcb5672d1d02ea11db
Aron <aron.kipkorir@student.moringaschool.com>
2024-06-27 15:16:46 +0300

    Adds loops

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+// writeCards
+
+function writeCards(names, event) {
+
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+
+        messages.push(message);
+    }
+    
+    return messages;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+//countdown
+
+function countDown() {
+    for (let i = 10; i >= 0; i--) {
+     console.log(i);
+    }
+    }
